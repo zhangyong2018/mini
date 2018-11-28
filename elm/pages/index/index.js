e53fe1b51be6c4e0e774604c70b506d96a8e59ba
zhangyong2018 <45409914+zhangyong2018@users.noreply.github.com>
2018-11-28 09:29:29 +0800

    elm
    elm

---
+++ elm/pages/index/index.js
@@ -0,0 +1,14 @@
+//index.js
+//获取应用实例
+const app = getApp()
+Page({
+  data: {
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1543218314130&di=3d32e975919c42144bc7bde3ac6e35fe&imgtype=0&src=http%3A%2F%2Fpic.90sjimg.com%2Fback_pic%2Fqk%2Fback_origin_pic%2F00%2F03%2F10%2F865d85ba89d775cf8579fff62ef8ae26.jpg',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1543218384691&di=cb2dd99ab477d60ecab9710951097aaf&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F01d1d257747cce0000012e7e28e6ef.jpg%402o.jpg'
+    ],
+
+  },
+})
+
